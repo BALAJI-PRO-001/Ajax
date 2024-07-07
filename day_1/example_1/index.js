38d4dc04e31f9eb26ab9808efe4a9e05a07b01b9
BALAJI-PRO-OO1 <emailForGIT000000@gmail.com>
2024-07-07 19:31:57 +0530

    Chor: Get request

---
+++ day_1/example_1/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const FILE_PATH = path.join(__dirname, "./index.html");
+
+app.get("/", (req, res) => {
+  res.sendFile(FILE_PATH);
+});
+
+app.get("/ajax", (req, res) => {
+  res.end("<h1> Response from express server </h1>");
+});
+
+app.listen(3000, () => console.log("Server is running ...."));
